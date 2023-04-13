74ba2943fba209cd30bf88bcc6b8cfb50ddbbf13
Thiago Maylon <tmaylon88@gmail.com>
2023-04-13 13:10:26 -0300

    adicionando a função de deletar task

--- src/components/ToDo/index.tsx
+++ src/components/ToDo/index.tsx
@@ -7,13 +7,19 @@ type Task = {
 
 
 export const ToDo = ({todo}: Task) => {
+    function handleTextObtained(text: string) {
+        console.log()
+        if (todo.indexOf(text) > -1) {
+            todo.splice(todo.indexOf(text), 1);
+          }
+    }
     return(
         <div className={style.todo}>
             <p className={style.p}>TO DO:</p>
 
             <div className={style.itens_todo}>
                 {todo.map((todos, index) => (
-                    <ToDoCard todo={todos} key={index}/>
+                    <ToDoCard todo={todos} key={index} onTextObtained={handleTextObtained}/>
                 ))}
             </div>
          
--- src/components/ToDoCard/index.tsx
+++ src/components/ToDoCard/index.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { IoMdTrash } from 'react-icons/io'
 import style from './style.module.css';
 
 interface props{
     todo: string,
+    onTextObtained: (text: string | null | undefined) => void
 }
 
-export const ToDoCard = ({todo, }: props) => {
+
+export const ToDoCard = ({todo, onTextObtained}: props) => {
     const [complet, setComplet] = useState<string>("")
+    const divRef = useRef<HTMLDivElement>(null); // Referência à div
 
     const handleClick = () => {
         complet == "complet" ? setComplet("") : setComplet("complet")
     }
-
+    const HandleClickDelet = () => {
+        const divText = divRef.current?.textContent;
+        onTextObtained(divText);
+    }
     return(
        <div className={style.Card} >
             <div className={style.radio_container}>
@@ -23,10 +29,11 @@ export const ToDoCard = ({todo, }: props) => {
                     <IoMdTrash 
                         size={20} 
                         color='#aaa'
+                        onClick={HandleClickDelet}
                     />
                 </div>
             </div>
-            <p className={style[complet]}>{todo}</p>
+            <p className={style[complet]} ref={divRef}>{todo}</p>
        </div>
     )
 }
